518f1be8f00e25e045789c0d8d10dac984dcc210
cubasve <eva.cubasvasquez@gmail.com>
2020-07-03 17:24:42 -0400

    Add purchase model for 1:M relationship

--- main_app/models.py
+++ main_app/models.py
@@ -14,3 +14,13 @@ class Brand(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'brand_id': self.id})
+
+
+class Purchase(models.Model):
+    date = models.DateField()
+    item = models.CharField(max_length=100)
+
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.item} on {self.date}"
